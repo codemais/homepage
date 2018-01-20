413fd1646fd8cd451a136b92deaf198e77afa33a
Fábio Santos <fabiosantos.pe@gmail.com>
2018-01-20 14:36:31 -0300

    filter para gerar resource do embed do video Youtube

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
-angular.module('app', [])
-  .controller('HomeController', function ($scope, $http) {
+var app = angular.module('app', [])
 
-    $http.get('data/episodes.json').then(function (result) {
-      $scope.episodes = result.data;
-    });
+app.controller('HomeController', function ($scope, $http) {
+  $http.get('data/episodes.json').then(function (result) {
+    $scope.episodes = result.data;
   });
+});
+
+app.filter('GetYouTubeUrl', function ($sce) {
+    return function (url) {
+      return $sce.trustAsResourceUrl(url);
+    };
+  }
+);
